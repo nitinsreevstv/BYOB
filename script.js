39c20f66f6fe8ac05d29e73a74d884483108376a
Priyanshu <priyanshu@voiceoc.com>
2025-02-05 15:56:33 +0530

    CSS Update

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.getElementById('booking-form').addEventListener('submit', function (e) 
     const email = document.getElementById('email').value;
     const date = document.getElementById('date').value;
     const time = document.getElementById('time').value;
-  
-    // You can add your logic here to handle the booking (e.g., send data to a server)
+
     alert(`Thank you, ${name}! Your booking for ${date} at ${time} has been received. We will confirm via email at ${email}.`);
-  });
+});
